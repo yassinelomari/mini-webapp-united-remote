1134d5ebc745b6eb0440143d9caec0e874bc8671
yassinelomari <yassinelomari95@gmail.com>
2019-04-01 10:14:41 +0100

    Adding list-repositories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {GithubRepositoryService} from './sharedServices/githubRepository.service';
+import { ListRepositoriesComponent } from './list-repositories/list-repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListRepositoriesComponent
   ],
   imports: [
     BrowserModule,
